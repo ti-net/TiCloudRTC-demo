e647483aaa57f0fdc5284a37ff8357cadfd93e09
zhoujiang <zhoujiang@ti-net.com.cn>
2023-08-28 15:14:42 +0800

    feat: update

--- Tools/TiCloudAccessToken/php/sample/sample.php
+++ Tools/TiCloudAccessToken/php/sample/sample.php
@@ -1,9 +1,9 @@
 <?php
 include("../src/AccessTokenBuilder.php");
 
-$enterpriseId = 7002485;
-$token = "cf49050d2c45ea69719a6a88d5f21bd8";
-$userId = "zhoujiang";
+$enterpriseId = 7000002;
+$token = "xxx";
+$userId = "xxx";
 $expireTimeInSeconds = 3600;
 
 $accessToken = AccessTokenBuilder::buildAccessToken($enterpriseId, $token, $userId, $expireTimeInSeconds);
