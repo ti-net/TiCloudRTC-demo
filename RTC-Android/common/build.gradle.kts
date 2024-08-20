62c850aae2bd782db6dcdf5192d86a937b3b1b07
Jarvis Semou <jarvis.semou@gmail.com>
2024-08-20 11:07:22 +0800

    feat:更新 RTC SDK 版本至 4.2.9

--- RTC-Android/common/build.gradle.kts
+++ RTC-Android/common/build.gradle.kts
@@ -105,7 +105,7 @@ dependencies {
     api("com.tencent.bugly:crashreport:4.1.9")
 
     // TiCloudRtc SDK
-    api("com.github.ti-net:TiCloud-RTC-Android:4.2.7")
+    api("com.github.ti-net:TiCloud-RTC-Android:4.2.9")
 
     // 观测云(可选日志上传)
 //    api("com.cloudcare.ft.mobile.sdk.tracker.agent:ft-sdk:1.5.0")
